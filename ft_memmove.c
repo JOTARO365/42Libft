a10e158a6534d2391e34f124293b60dfa75c66be
Jotaro <joewisanu@github.com>
2023-09-27 07:10:34 +0700

    ft_memmove.c

---
+++ ft_memmove.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: waon-in <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 15:35:29 by waon-in           #+#    #+#             */
+/*   Updated: 2023/09/17 15:57:59 by waon-in          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t			i;
+	unsigned char		*d;
+	unsigned char		*s;
+
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	i = 0;
+	if (d < s)
+	{
+		while (i < len)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	else if (d > s)
+	{
+		i = len;
+		while (i > 0)
+		{
+			i--;
+			d[i] = s[i];
+		}
+	}
+	return (dst);
+}
---
+++ ft_strlcpy.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+#include <stdio.h>
+
+size_t	strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t		i;
+	unsigned char	*d;
+	unsigned char	*s;
+
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	i = 0;
+	while (src[i])
+	{
+		i++;
+	}
+	if (i
+}
