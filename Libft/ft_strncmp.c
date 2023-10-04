8900ef98b8087bde00de800e43a5da40996b9d29
Wisanu Aon-In <108912837+JOTARO365@users.noreply.github.com>
2023-10-04 21:32:04 +0700

    Delete Libft directory

--- Libft/ft_strncmp.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-
-int	strncmp(const char *s1, const char *s2, size_t len)
-{
-}
--- Libft/ft_tolowwer.c
+++
@@ -1,8 +0,0 @@
-#include "libft.h"
-
-int	ft_tolowwer(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		c += 32;
-	return (c);
-}
